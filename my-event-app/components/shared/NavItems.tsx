8c1ad7f67ca4068cc5631d9b29777cc5e7392f0b
Lingchao Zhang <zhanglingchao2021@gmail.com>
2024-01-09 14:55:50 +1100

    Header-Nav-Footer views added: 1. create a Header to display icons, nav and user. 2. create a Footer. 3. create a MobileNav to display nav items in small screen. 4. create a NavItems to display nav items. 5. add a constant to display content conveniently. 6. add two new components from shadcn ui.

---
+++ my-event-app/components/shared/NavItems.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { headerLinks } from "@/constants"
+import Link from "next/link"
+import { usePathname } from 'next/navigation'
+const NavItems = () => {
+    const pathname = usePathname()
+    return(
+        <ul className="flex flex-col items-start gap-5 md:flex-row">
+            {
+                headerLinks.map((headerLink) => {
+                    const isActive = pathname === headerLink.route
+                    return(
+                        <li 
+                            key={headerLink.label} 
+                            className={`${isActive ? "text-primary-500" : ""} flex-center p-medium-16 whitespace-nowrap`}
+                        >
+                            <Link href={headerLink.route}>{headerLink.label}</Link>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+export default NavItems
